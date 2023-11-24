63859c01c661ae29f44d634b49991f579085dad6
antubhatta <antubhatta09@gmail.com>
2023-11-24 22:33:53 +0600

    banner section added successfully

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <li><a>Item 3</a></li>
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar fixed z-10 container mx-auto bg-opacity-30 bg-black text-white">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
